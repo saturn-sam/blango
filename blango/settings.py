4ff1601a828847f3cf9234007a9e7fb73c90cbb3
Samrat zaman.iu21.ali1 <zaman.iu21.ali1@gmail.com>
2024-08-28 09:21:10 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -149,6 +149,16 @@ class Dev(Configuration):
         ),
     }
 
+
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
+
+
     # Password validation
     # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
